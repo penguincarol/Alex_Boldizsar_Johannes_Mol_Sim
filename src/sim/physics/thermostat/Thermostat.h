9bf8429ace6f907b65ffd283f3fadf503b1790c2
Johannes Riemenschneider <johannes.riemenschneider@tum.de>
2023-01-23 16:49:29 +0100

    whoops thermostat

--- src/sim/physics/thermostat/Thermostat.h
+++ src/sim/physics/thermostat/Thermostat.h
@@ -49,7 +49,7 @@ public:
                         unsigned int cT = default_n_term, unsigned int dimensions = default_dims,
                         double dT = default_delta_temp, double TInit = default_t_init,
                         bool thermoEnable = default_therm, ThermoMode tm = ThermoMode::normal)
-                        : pc(particleContainer), countThreshold(cT), deltaTemp(dT), Ttarget(T_t), dims(dimensions), thermoMode(tm) {
+                        : pc(particleContainer), countThreshold(cT), dims(dimensions), deltaTemp(dT), Ttarget(T_t),  thermoMode(tm) {
         if(tm == ThermoMode::normal){
             numberFlowingParticles = pc.activeSize();
         }else{

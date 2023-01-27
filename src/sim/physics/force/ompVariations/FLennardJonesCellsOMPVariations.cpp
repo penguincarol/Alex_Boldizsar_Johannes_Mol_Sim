3d6bb707214f8093c037e53bd1e12642f9aa1539
Johannes Riemenschneider <johannes.riemenschneider@tum.de>
2023-01-27 11:50:21 +0100

    seems good now, distribution variants (round robin, greedy) not tested yet, membrane support not tested yet. doing that today. but i don't think that should be an issue

--- src/sim/physics/force/ompVariations/FLennardJonesCellsOMPVariations.cpp
+++ src/sim/physics/force/ompVariations/FLennardJonesCellsOMPVariations.cpp
@@ -42,7 +42,7 @@ std::vector<T> flatten(const std::vector<std::vector<T>> &orig)
                 for(size_t i = 0; i < cell.size(); i++){
                     for(size_t j = i+1; j < cell.size(); j++){
                         //interactions++;
-                        fPairFun(force, x, eps, sig, m, type, i, j);
+                        fPairFun(force, x, eps, sig, m, type, cell[i], cell[j]);
                     }
                 }
             }
@@ -62,18 +62,27 @@ std::vector<T> flatten(const std::vector<std::vector<T>> &orig)
                                                        ParticleContainer::VectorCoordWrapper& cells,
                                                        std::vector<double> &eps,
                                                        std::vector<double> &sig){
+            static const double rt3_2 = std::pow(2, 1.0 / 3.0);
             auto fPairFun = this->fpairFun;
             #pragma omp declare reduction(vec_double_plus : std::vector<double> : \
                               std::transform(omp_out.begin(), omp_out.end(), omp_in.begin(), omp_out.begin(), std::plus<double>())) \
                     initializer(omp_priv = decltype(omp_orig)(omp_orig.size()))
 
-            #pragma omp parallel for default(none) shared(fPairFun, oldForce, x, v, m, type, count, cells, eps, sig, tasks) reduction(vec_double_plus : force)
+            #pragma omp parallel for default(none) shared(rt3_2, fPairFun, oldForce, x, v, m, type, count, cells, eps, sig, tasks) reduction(vec_double_plus : force)
             for(auto& [cellIndexI, cellIndexJ]: tasks) {
                 size_t cII = cellIndexI;     //openMP problems with the other variant
                 size_t cIJ = cellIndexJ;
 
                 for (auto pIndexI: cells[cII]) {
                     for (auto pIndexJ: cells[cIJ]) {
+
+                        auto dsqr = ((x[3*pIndexI + 0]-x[3*pIndexJ+0])*(x[3*pIndexI + 0]-x[3*pIndexJ+0]))+
+                                ((x[3*pIndexI + 1]-x[3*pIndexJ+1])*(x[3*pIndexI + 1]-x[3*pIndexJ+1]))+
+                                ((x[3*pIndexI + 2]-x[3*pIndexJ+2])*(x[3*pIndexI + 2]-x[3*pIndexJ+2]));
+                        auto sigma2 = (sig[pIndexI] + sig[pIndexJ])*(sig[pIndexI] + sig[pIndexJ]) / 4;  //sigma^2
+                        if (type[pIndexI] & 0x80000000 || type[pIndexJ] & 0x80000000) {
+                            if (dsqr >= rt3_2 * sigma2) continue;
+                        }
                         fPairFun(force, x, eps, sig, m, type, pIndexI, pIndexJ);
                         //interactionsDistinctCells++;
                     }
@@ -109,7 +118,7 @@ std::vector<T> flatten(const std::vector<std::vector<T>> &orig)
                 for (size_t i = 0; i < cell.size(); i++) {
                     for (size_t j = i + 1; j < cell.size(); j++) {
                         //interactions++;
-                        fPairFun(force, x, eps, sig, m, type, i, j);
+                        fPairFun(force, x, eps, sig, m, type, cell[i], cell[j]);
                     }
                 }
             }
@@ -140,8 +149,18 @@ std::vector<T> flatten(const std::vector<std::vector<T>> &orig)
                                                         ParticleContainer::VectorCoordWrapper &cells,
                                                         std::vector<double> &eps,
                                                         std::vector<double> &sig) {
+                        static const double rt3_2 = std::pow(2, 1.0 / 3.0);
                         for (auto pIndexI: cells[cII]) {
                             for (auto pIndexJ: cells[cIJ]) {
+
+                                auto dsqr = ((x[3*pIndexI + 0]-x[3*pIndexJ+0])*(x[3*pIndexI + 0]-x[3*pIndexJ+0]))+
+                                            ((x[3*pIndexI + 1]-x[3*pIndexJ+1])*(x[3*pIndexI + 1]-x[3*pIndexJ+1]))+
+                                            ((x[3*pIndexI + 2]-x[3*pIndexJ+2])*(x[3*pIndexI + 2]-x[3*pIndexJ+2]));
+                                auto sigma2 = (sig[pIndexI] + sig[pIndexJ])*(sig[pIndexI] + sig[pIndexJ]) / 4;  //sigma^2
+                                if (type[pIndexI] & 0x80000000 || type[pIndexJ] & 0x80000000) {
+                                    if (dsqr >= rt3_2 * sigma2) continue;
+                                }
+
                                 fPairFun(force, x, eps, sig, m, type, pIndexI, pIndexJ);
                             }
                         }
